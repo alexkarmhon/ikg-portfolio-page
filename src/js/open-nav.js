76e4e8b6af3114fb97f7034b0006d3bcb4ced910
alexkarmhon <89stigmat89@gmail.com>
2025-01-18 16:41:37 +0200

    Header

---
+++ src/js/open-nav.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('nav-menu');
+const openButton = document.getElementById('open-nav-btn');
+const closeButton = document.getElementById('close-nav-btn');
+
+const openMenu = () => {
+  menu.style.display = 'block';
+};
+
+const closeMenu = () => {
+  menu.style.display = 'none';
+};
+
+openButton.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
