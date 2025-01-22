577b746f745664dfbc8ad2f5f7ec9f3d5d89b6b9
alexkarmhon <89stigmat89@gmail.com>
2025-01-22 18:17:58 +0200

    Portfolio section

--- src/js/open-nav.js
+++ src/js/open-nav.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('nav-menu');
-const openButton = document.getElementById('open-nav-btn');
-const closeButton = document.getElementById('close-nav-btn');
+// const openButton = document.getElementById('open-nav-btn');
+// const closeButton = document.getElementById('close-nav-btn');
 
 const openMenu = () => {
   menu.style.display = 'block';
@@ -10,5 +10,8 @@ const closeMenu = () => {
   menu.style.display = 'none';
 };
 
-openButton.addEventListener('click', openMenu);
-closeButton.addEventListener('click', closeMenu);
+// openButton.addEventListener('click', openMenu);
+// closeButton.addEventListener('click', closeMenu);
+
+window.closeMenu = closeMenu;
+window.openMenu = openMenu;
