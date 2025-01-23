665c28d163e30d686f39b4dec94b378eb504dec7
alexkarmhon <89stigmat89@gmail.com>
2025-01-23 21:52:41 +0200

    About section

--- src/js/open-nav.js
+++ src/js/open-nav.js
@@ -1,6 +1,4 @@
 const menu = document.getElementById('nav-menu');
-// const openButton = document.getElementById('open-nav-btn');
-// const closeButton = document.getElementById('close-nav-btn');
 
 const openMenu = () => {
   menu.style.display = 'block';
@@ -9,9 +7,6 @@ const openMenu = () => {
 const closeMenu = () => {
   menu.style.display = 'none';
 };
-
-// openButton.addEventListener('click', openMenu);
-// closeButton.addEventListener('click', closeMenu);
 
 window.closeMenu = closeMenu;
 window.openMenu = openMenu;
